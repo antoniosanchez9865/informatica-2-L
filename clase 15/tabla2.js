dc8c4f8e8061c8dd07b45bbcb8d96996cbfdaa39
Antonio Sánchez <antoniojsanchezb@gmail.com>
2021-02-05 15:48:32 -0500

    prueba

--- clase 15/tabla2.js
+++ clase 15/tabla2.js
@@ -1,11 +1,12 @@
 Tabla= 
 //lista de alimentos 
-"Lista e alimentos" =  
-{nombre : "Arroz", vitaminas : "B1, B12", calorias : 130}
-{nombre ; "Huevos"; vitaminas ; "B2, B6, B12"; calorias; 155}
-{nombre ; "Carne"; vitaminas; "B12"; calorias; 143}
-{onmbre ; "Pescado"; vitaminas; "A, D"; calorias; 206 }
-{nombre ; "pan", vitaminas , "B1, B2 B6", calorias ; 265}
+listaAlimentos =  [
+    {nombre : "Arroz", vitaminas : "B1, B12", calorias : 130},
+{nombre : "Huevos", vitaminas : "B2, B6, B12", calorias: 155},
+{nombre : "Carne", vitaminas: "B12", calorias: 143},
+{onmbre : "Pescado", vitaminas: "A, D",calorias: 206 },
+{nombre : "pan", vitaminas : "B1, B2 B6", calorias : 265}]
+
 deje    =  documento . querySelector ( '#tabla' )
 // console.log (tabla)
 let  contenidoTabla  =  '<tr> <th> Nombre </th> <th> Diámetro </th> <th> Imagen </th> </tr>'
